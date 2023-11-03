75f0588a9d6db741f860246b760c2b6a7d64826b
Keks <keks@htmlacademy.ru>
2023-11-03 13:57:37 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,3 @@
+import {createPicturesData} from './data.js';
+import {createThumbnails} from './thumbnails.js';
+createThumbnails(createPicturesData());
---
+++ 9/js/util.js
@@ -0,0 +1,28 @@
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const result = Math.floor(Math.random() * (max - min + 1)) + min;
+  return result;
+};
+
+const createUnicueIds = (min, max) => {
+  const previousIds = [];
+  return function () {
+    let currentId = getRandomInt(min, max);
+    if (previousIds.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousIds.includes(currentId)) {
+      currentId = getRandomInt(min, max);
+    }
+    previousIds.push(currentId);
+    return currentId;
+  };
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.length - 1)];
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export {getRandomInt, createUnicueIds, getRandomArrayElement, isEscapeKey};
+
