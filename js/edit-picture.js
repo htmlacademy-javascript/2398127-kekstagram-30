931b81b6cb867b585ed9ed71f2ee1cca737cca4a
ArsiRex <arsirex@yandex.ru>
2023-11-16 18:33:12 +0300

    Добавил эффекты и изменение масштаба для изображений

--- js/edit-picture.js
+++ js/edit-picture.js
@@ -1,10 +1,16 @@
+const ScaleInfo = {
+  DEFAULT_SCALE: 100,
+  MINIMAL_SCALE: 25,
+  MAX_SCALE: 100,
+  STEP: 25
+};
 const imgUpload = document.querySelector('.img-upload');
 const scaleControlSmaller = imgUpload.querySelector('.scale__control--smaller');
 const scaleControlBigger = imgUpload.querySelector('.scale__control--bigger');
 const scaleControlValue = imgUpload.querySelector('.scale__control--value');
 const imgPreview = imgUpload.querySelector('.img-upload__preview img');
 const effectLevelSlider = imgUpload.querySelector('.effect-level__slider');
-const effectLevelValue = imgUpload.querySelector('.effect-level__value');
+const effectLevel = imgUpload.querySelector('.effect-level__value');
 const effectsContainer = imgUpload.querySelector('.effects__list');
 const sliderContainer = imgUpload.querySelector('.img-upload__effect-level');
 const effectInfo = {
@@ -48,107 +54,123 @@ const effectInfo = {
   },
 };
 
-const scaleInfo = {
-  DEFAULT_SCALE: 100,
-  MINIMAL_SCALE: 25,
-  MAX_SCALE: 100,
-  STEP: 25
-};
-
+/**
+ * Функция проверяет масштаб изображения и уменьшает его если масштаб больше минимального разрешенного.
+ */
 const getSmallerPicture = () => {
   const oldScale = parseInt(scaleControlValue.value, 10);
-  if (oldScale >= 50) {
-    const newScale = oldScale - scaleInfo.STEP;
+  if (oldScale > ScaleInfo.MINIMAL_SCALE) {
+    const newScale = oldScale - ScaleInfo.STEP;
     imgPreview.style.transform = `scale(${newScale / 100})`;
     scaleControlValue.value = `${newScale}%`;
-
   }
 };
 
+/**
+ * Функция проверяет масштаб изображения и увеличивает его если масштаб меньше максимального разрешенного.
+ */
 const getBiggerPicture = () => {
   const oldScale = parseInt(scaleControlValue.value, 10);
-  if (oldScale <= 75) {
-    const newScale = oldScale + scaleInfo.STEP;
+  if (oldScale < ScaleInfo.MAX_SCALE) {
+    const newScale = oldScale + ScaleInfo.STEP;
     imgPreview.style.transform = `scale(${newScale / 100})`;
     scaleControlValue.value = `${newScale}%`;
-
   }
 };
 
+/**
+ * Функция сбрасывает масштаб изображения.
+ */
 const resetScale = () => {
-  imgPreview.style.transform = `scale(${scaleInfo.DEFAULT_SCALE / 100})`;
+  imgPreview.style.transform = `scale(${ScaleInfo.DEFAULT_SCALE / 100})`;
+  scaleControlValue.value = `${ScaleInfo.DEFAULT_SCALE}%`;
 };
 
-const initScaleButton = () => {
+/**
+ * Функция добавляет обработчики событий на клик кнопок увеличения и уменьшения масштаба.
+ */
+const initScale = () => {
   scaleControlSmaller.addEventListener('click', getSmallerPicture);
   scaleControlBigger.addEventListener('click', getBiggerPicture);
 };
 
+let currentEffect = Object.keys(effectInfo)[5];
 
-let currentEffect = effectInfo.none;
-
-const updateEffect = () => {
-  const effectLevel = effectLevelSlider.noUiSlider.get();
-  const effect = effectInfo.currentEffect;
-  effectLevelValue.value = effectLevel;
-  if (currentEffect.unit) {
-    imgPreview.style.filter = `${effect}(${effectLevel}${currentEffect.unit})`;
+/**
+ * Функция записывает значение интенсивности эффекта со слайдера в скрытое поле и накладывает эффект на изображение.
+ */
+const updateEffectOnPicture = () => {
+  const currentEffectLevel = effectLevelSlider.noUiSlider.get();
+  effectLevel.value = currentEffectLevel;
+  if (effectInfo[currentEffect].unit) {
+    imgPreview.style.filter = `${effectInfo[currentEffect].effect}(${currentEffectLevel}${effectInfo[currentEffect].unit})`;
   } else {
-    imgPreview.style.filter = `${effect}(${effectLevel})`;
+    imgPreview.style.filter = `${effectInfo[currentEffect].effect}(${currentEffectLevel})`;
   }
 };
 
-const createEffectSlider = (effect) => {
+/**
+ * Функция создает слайдер под изображением.
+ * @param {number} min - минимальное значение слайдера.
+ * @param {number} max - максимальное значение слайдера.
+ * @param {number} step - шаг слайдера.
+ */
+const createEffectSlider = ({min, max, step}) => {
   noUiSlider.create(effectLevelSlider, {
-    range: {
-      min: effect.min,
-      max: effect.max,
-    },
-    start: effect.max,
-    step: effect.step,
+    range: {min, max},
+    start: max,
+    step,
     connect: 'lower',
   });
-
-  effectLevelSlider.noUiSlider.on('update', updateEffect);
+  effectLevelSlider.noUiSlider.on('update', updateEffectOnPicture);
 };
 
-const updateEffectSlider = (effect) => {
-  effectLevelSlider.noUiSlider.updateOptions(effectLevelSlider, {
-    range: {
-      min: effect.min,
-      max: effect.max,
-    },
-    start: effect.max,
-    step: effect.step,
+/**
+ * Функция обновляет данные слайдера.
+ * @param {number} min - минимальное значение слайдера.
+ * @param {number} max - максимальное значение слайдера.
+ * @param {number} step - шаг слайдера.
+ */
+const updateEffectSlider = ({min, max, step}) => {
+  effectLevelSlider.noUiSlider.updateOptions({
+    range: {min, max},
+    start: max,
+    step,
   });
 };
 
+/**
+ * Функция обработчика событий, которая обновляет слайдер в зависимости от выбранного эффекта для изображения.
+ */
 const onEffectChange = (evt) => {
-  currentEffect = evt.target.value;
+  if (evt.target.matches('input[type="radio"]')) {
+    currentEffect = evt.target.value;
+  }
   if (currentEffect === 'none') {
-    //sliderContainer.classList.add('hidden');
+    sliderContainer.classList.add('hidden');
     imgPreview.style.filter = null;
     return;
   }
-  updateEffectSlider(effectInfo.currentEffect);
+  updateEffectSlider(effectInfo[currentEffect]);
   sliderContainer.classList.remove('hidden');
 };
 
-const addEffectOptions = () => {
-  //sliderContainer.classList.add('hidden');
-  createEffectSlider(currentEffect);
+/**
+ * Функция инициализирует добавление эффектов на изображение.
+ */
+const initEffectOptions = () => {
+  sliderContainer.classList.add('hidden');
+  createEffectSlider(effectInfo[currentEffect]);
   effectsContainer.addEventListener('click', onEffectChange);
 };
 
-addEffectOptions();
-initScaleButton();
+/**
+ * Функция сбрасывает эффекты по умолчанию.
+ */
+const removeEffects = () => {
+  currentEffect = Object.keys(effectInfo)[5];
+  updateEffectSlider(effectInfo[currentEffect]);
+  imgPreview.style.filter = null;
+};
 
-// noUiSlider.create(effectLevelSlider, {
-//   range: {
-//     min: 1,
-//     max: 100,
-//   },
-//   start: 1,
-//   step: 1,
-//   connect: 'lower',
-// });
+export {initEffectOptions, removeEffects, resetScale, initScale};
